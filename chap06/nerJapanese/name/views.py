5fefcf6f6c23d24f9d6ccc18d4bdc25a8b60e4e4
outsider-kithy <kishida@outsider-design-tech.net>
2025-04-08 13:50:21 +0900

    chap06修正

--- chap06/nerJapanese/name/views.py
+++ chap06/nerJapanese/name/views.py
@@ -26,29 +26,29 @@ def index():
 	outputs = ner_pipeline(text)
 	print(outputs)
 
-	# # entityが「B-法人名」もしくは「I-法人名」のワードを抽出してnames_tokens配列に格納
-	# names_tokens = []
-	# for output in outputs:
-	# 	if output['entity'] == 'B-法人名' or output['entity'] == 'I-法人名':
-	# 		names_tokens.append(output['word'])
-	# 	else:
-	# 		continue
 
-	# # entityが「B-法人名」の後に続く「I-法人名」をentityに持つ単語を結合し、固有名詞として配列resultsに格納
-	# results = []
-	# for i in range(len(outputs)):
-	# 	if(outputs[i]['entity'] == 'B-法人名'):
-	# 		name = outputs[i]['word']
-	# 		for j in range(i+1, len(outputs)):
-	# 			if outputs[j]['entity'] == 'I-法人名':
-	# 				# #を取り除いて結合
-	# 				name += outputs[j]['word'].replace("#", "")
-	# 			else:
-	# 				break
-	# 		results.append(name)
+	# # entityが「B-人名」もしくは「I-人名」のワードを抽出してnames_tokens配列に格納
+	names_tokens = []
+	for output in outputs:
+		if output['entity'] == 'B-人名' or output['entity'] == 'I-人名':
+			names_tokens.append(output['word'])
+		else:
+			continue
+
+	# entityが「B-人名」の後に続く「I-人名」をentityに持つ単語を結合し、固有名詞として配列resultsに格納
+	results = []
+	for i in range(len(outputs)):
+		if(outputs[i]['entity'] == 'B-人名'):
+			name = outputs[i]['word']
+			for j in range(i+1, len(outputs)):
+				if outputs[j]['entity'] == 'I-人名':
+					# #を取り除いて結合
+					name += outputs[j]['word'].replace("#", "")
+				else:
+					break
+			results.append(name)
 
 	# # resultsをjson形式で返す
-	# return json.dumps(results, ensure_ascii=False)
+	return json.dumps(results, ensure_ascii=False)
 
-	return "Name route"
 
